d218beb4738f02937d435fe8da9bbf732825a399
topleft <pete.james.jeffryes@gmail.com>
2015-08-23 17:35:50 -0600

    map showing on location click inside list

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,42 @@
 // add scripts
 
+function initMap(elementId){
+  var map = new google.maps.Map(document.getElementById(elementId), {
+      center: {lat:39.393981, lng:-106.016311},
+      zoom: 7
+    });
+  return map;
+};
+
+
+function initAutoComplete(inputId){
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById(inputId));
+  return autocomplete;
+}
+
+function autoListener(autocomplete){
+    var place;
+    autocomplete.addListener('place_changed', function() {
+    place = autocomplete.getPlace();
+    return place;
+  });
+  return place;
+}
+
+
 $(document).on('ready', function() {
   console.log('sanity check!');
+  var autocomplete = initAutoComplete("location-input");
+
+  place = autoListener(autocomplete);
+
+  $(".location").on("click", function(){
+    $(".toggle").show();
+    var map = initMap("map");
+  });
+
+  //create table row for map with unique ID
+
+
+
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var todos = [];
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ToDo List' });
 });
+
+router.post("/submit", function(req, res){
+  var itemTitle = req.body.itemTitle;
+  var itemDetails = req.body.itemDetails;
+  var itemLocation = req.body.itemLocation;
+  todos.push({title: itemTitle, details: itemDetails, location: itemLocation});
+  res.render('list', {title: 'ToDo List', todos: todos})
+});
+
+
+
 
 module.exports = router;
